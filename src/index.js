da1f18444fc702f755ee9faf44f4dd3466fdaade
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-07 02:44:11 +0200

    feedback part done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import { Feedback } from 'components/feedback/feedback';
+import { Feedback } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Feedback />
-    <App />
   </React.StrictMode>
 );
